7f061322afab2d9c9b2ae64002342f8898c5468e
Maximiliano Leonardo García Santanna <maximiliano.garcia.santanna@gmail.com>
2024-06-28 17:11:26 -0300

    Update from the about menu available to windows too!

--- screens/popups/version_dialog/version.lua
+++ screens/popups/version_dialog/version.lua
@@ -26,21 +26,28 @@ function M.get_latest()
 	end)
 end
 
-function M.check_version()
+function M.get_version_information()
 	-- Call this through a flow
 	M.BUSY = true
-	local current_version = M.current_version()
+	local version_information = {}
+	version_information.current = M.current_version()
 	if M.releases == nil then
 		M.get_latest()
 		flow.until_true(function() return not M.BUSY end)
 	end
+
+	version_information.latest = M.releases.latest
+	version_information.latest_number = M.releases[M.releases.latest].number
+	version_information.latest_url = M.releases[M.releases.latest].url
+	version_information.latest_win_url = M.releases[M.releases.latest].win_url
+	version_information.current_number = M.releases[version_information.current] and M.releases[version_information.current].number
+	version_information.up_to_date = version_information.current == M.releases.latest
 	
-	if M.releases[current_version] == nil then
+	if version_information.current_number == nil then
 		log.info("Current version not in version json")
-		return
 	end
 
-	return M.releases.latest == current_version, M.releases[M.releases.latest].number - M.releases[current_version].number, M.releases[M.releases.latest].url
+	return version_information
 end
 
 function M.current_version()
