d6a4aaca3e73ba971a8806ad2f00d14e56020827
maximilianolgs <maximilianolgs@users.noreply.github.com>
2024-09-12 13:38:12 -0300

    Fix export pokemon errors for html roll20 and button (#157)
    * Fix export pokemon errors for html #148
    - Exporting now works
    - Fix to Roll20 sheet fomat
    - Fix on errors thrown when pressing the clipboard or roll20 buttons

--- utils/html_clipboard.lua
+++ utils/html_clipboard.lua
@@ -1,4 +1,5 @@
 local flow = require "utils.flow"
+local log = require "utils.log"
 
 local M = {}
 
@@ -6,14 +7,28 @@ local copy_clipboard_callback
 local paste_clipboard_callback
 
 local function js_listener(self, message_id, message)
+	local e = nil
 	if message_id == "clipboard_copy" then
 		if copy_clipboard_callback then
 			copy_clipboard_callback(message)
+		else
+			e = "Missing copy_clipboard_callback"
 		end
 	elseif message_id == "clipboard_paste" then
 		if paste_clipboard_callback then
 			paste_clipboard_callback(message)
+		else
+			e = "Missing paste_clipboard_callback"
 		end
+	else
+		e = "Unhandled message_id " .. message_id
+	end
+	if e then
+		gameanalytics.addErrorEvent {
+			severity = gameanalytics.SEVERITY_CRITICAL,
+			message = e
+		}
+		log.fatal(e)
 	end
 end
 
@@ -22,6 +37,8 @@ jstodef.add_listener(js_listener)
 -- copy str to the clipboard
 function M.copy(value, callback)
 	copy_clipboard_callback = callback
+	value = value:gsub("'", "\\'")
+	value = value:gsub("\\n", "\\\\n")
 	html5.run("clipboard_copy('" .. value .. "')")
 end
 
